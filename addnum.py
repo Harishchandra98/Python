818ed76d3473e2407225b6589eb52e35fdcfb2ea
Harishchandra98 <harishchandrads98@gmail.com>
2021-06-24 12:54:19 +0530

    adding two numbers

---
+++ addnum.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return (a+b);
+
+a = int(input(" Enter the first number : "));
+b = int(input(" Enter the second number : "));
+
+#print ("type of a",type(a));
+c = add(a,b);
+
+print (" total : ",c);
+
+#This is program for adding two numbers
+
+
